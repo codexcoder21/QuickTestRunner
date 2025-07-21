61f859f9318980e52414408a1133d0bc388f55d3
User <user@Mac.localdomain>
2025-07-20 23:04:24 -0700

    add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,14 @@ group = "community.kotlin.test.quicktest"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    maven {
+        url = uri("http://kotlin.directory")
+        metadataSources {
+            mavenPom()
+            artifact()
+        }
+        isAllowInsecureProtocol = true
+    }
     mavenCentral()
 }
 
@@ -15,6 +23,8 @@ dependencies {
     implementation("commons-cli:commons-cli:1.5.0")
     implementation("org.apache.commons:commons-text:1.10.0")
     implementation("com.squareup.okio:okio:3.6.0")
+    implementation("kompile-cli:kompile-cli:0.0.1")
+    implementation("community.kotlin.psi.annotationutils:community-kotlin-psi-annotationutils:0.0.1")
     testImplementation(kotlin("test"))
     testImplementation("org.eclipse.jdt:ecj:3.33.0")
 }
